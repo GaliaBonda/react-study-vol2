d823e53eda9c72f644204d32349649946a4b6f13
GaliaBonda <galina.rizhniak@gmail.com>
2022-06-24 10:29:30 +0300

    api got token, but no boards: 401

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,13 +1,55 @@
 import axios from 'axios';
 import { api } from '../common/constants';
 
+// let token;
+
+// const config = {
+//   headers: { Authorization: `Bearer ${token}` }
+// };
+
+// const bodyParameters = {
+//  key: "value"
+// };
+
+// axios.post( 
+// 'http://localhost:8000/api/v1/get_token_payloads',
+// bodyParameters,
+// config
+// ).then(console.log).catch(console.log);
+
+// let token;
+
+// token = await api.post(`/board`, board);
+
 const instance = axios.create({
   baseURL: api.baseURL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer 123',
+    // 'Authorization': `Bearer ${token}`,
   },
 });
+
+// let token: string;
+
+// async function getToken() {
+//   let res = await instance.post('/user', {"email": "test@gmail.com", "password": "testpass"});
+//   console.log(res);
+  
+//   return res;
+// } 
+
+
+
+// instance.interceptors.request.use(
+//   async config => {
+//     let token = await getToken();
+//     if(config.headers) config.headers['Authorization'] = `Bearer ${token}`;
+//         return config;
+//     },
+//     error => {
+//         return Promise.reject(error);
+//     }
+// );
 
 instance.interceptors.response.use((res) => res.data);
 
--- src/store/modules/boards/actions.ts
+++ src/store/modules/boards/actions.ts
@@ -1,10 +1,21 @@
 // import api from '/src/api';
-// import config from '/src/common/constants/api';
+import config from '../../../common/constants/api';
 import {Dispatch} from "redux";
 import api from "../../../api";
+import { AxiosResponse } from 'axios';
 
 export const getBoards = () => async (dispatch: Dispatch) => {
     try {
+        let res: AxiosResponse & {accessToken: string} = await api.post('/login', {
+            email: "test@gmail.com", password: "testpass"
+        });
+        api.interceptors.request.use(function (config) {
+            const token = res.accessToken;
+            if (config.headers) config.headers.Authorization =  token;
+        
+            return config;
+        });
+        console.log(res.accessToken);
         const data = await api.get("/board");
         dispatch({type: 'UPDATE_BOARDS', payload: data});
     } catch (e) {
@@ -12,3 +23,5 @@ export const getBoards = () => async (dispatch: Dispatch) => {
         dispatch({type: 'ERROR_ACTION_TYPE'});
     }
 }
+
+
