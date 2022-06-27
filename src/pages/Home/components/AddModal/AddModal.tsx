c8b34b6b43c48a6ce6db71d1fca8e53085215e6b
GaliaBonda <galina.rizhniak@gmail.com>
2022-06-27 12:32:41 +0300

    modal styled

--- src/pages/Home/components/AddModal/AddModal.tsx
+++ src/pages/Home/components/AddModal/AddModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './addmodal.scss';
 
 type propsType = {
     title: string;
@@ -21,10 +22,17 @@ export default function AddModal(props: propsType) {
     // if (!shown) return null;
     return (<div className="add-modal">
         <div className="modal-overlay">
-            <h2 className="add-modal__title">{props.title}</h2>
-            <input type="text" className="add-modal__input" placeholder='New board name' />
-            <button className="modal__btn btn" onClick={handleClick}>Cancel</button>
-            <button className="modal__btn btn">Ok</button>
+            <div className="add-modal__main">
+                <h2 className="add-modal__title">{props.title}</h2>
+                <label className="add-modal__label">
+                    <input type="text" className="add-modal__input" placeholder='New board name' />
+                </label>
+                <div className="add-modal__btns">
+                    <button className="add-modal__btn btn" onClick={handleClick}>Cancel</button>
+                    <button className="add-modal__btn btn">Ok</button>
+                </div>
+
+            </div>
         </div>
     </div>);
 }
