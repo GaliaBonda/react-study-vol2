799be0234dc439e337f8415440eaf6685b7d0de3
GaliaBonda <gb@seo.kr.ua>
2022-08-01 08:21:01 +0300

    progress bar still

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -7,19 +7,31 @@ type PropsType = {
 }
 
 
+let interval: NodeJS.Timer;
+let secondInterval: NodeJS.Timer;
+let timeout: NodeJS.Timeout;
 
 export default function ProgressBar(props: PropsType) {
     const [dynamicWidth, setDynamicWidth] = useState(0);
     const [barIsVisible, setBarVisible] = useState(false);
 
     useEffect(() => {
+        // setDynamicWidth((val) => {
+        //     const newVal = val + 1;
+        //     console.log('fff', val);
+        //     if (newVal > 99) {
+        //         clearInterval(interval);
+        //     }
+        //     return newVal;
+        // });
         // console.log('progress bar use effect');
-        let interval: NodeJS.Timer;
+        // let interval: NodeJS.Timer;
         const progressInterceptor = api.interceptors.request.use((config) => {
             if (config.method !== 'get') return config;
             interval = setInterval(() => {
                 setDynamicWidth((val) => {
                     const newVal = val + 1;
+                    console.log('fff', val);
                     if (newVal > 99) {
                         clearInterval(interval);
                     }
@@ -29,32 +41,94 @@ export default function ProgressBar(props: PropsType) {
             // console.log(config);
             return config;
         });
-        let secondInterval: NodeJS.Timer;
-        let timeout: NodeJS.Timeout;
-        const progressEndInterceptor = api.interceptors.response.use((res) => {
-            if ('accessToken' in res) return res;
-            timeout = setTimeout(() => {
-                secondInterval = setInterval(() => {
-                    setDynamicWidth((val) => {
-                        const newVal = val + 1;
-                        if (newVal > 99) {
-                            clearInterval(secondInterval);
-                            clearTimeout(timeout);
-                        }
-                        return newVal;
-                    });
-                });
-            }, 1000000);
-            // clearTimeout(timeout);
-            return res;
-        });
+
+
 
         return () => {
             api.interceptors.request.eject(progressInterceptor);
-            api.interceptors.request.eject(progressEndInterceptor);
+            // api.interceptors.response.eject(progressEndInterceptor);
             // clearInterval(interval);
             // clearInterval(secondInterval);
-            clearTimeout(timeout);
+            // clearTimeout(timeout);
+        }
+    });
+
+    useEffect(() => {
+        const progressEndInterceptor = api.interceptors.response.use((res) => {
+            if ('accessToken' in res) return res;
+            // console.log(dynamicWidth);
+
+            const promise = new Promise((resolve) => {
+                timeout = setTimeout(() => {
+                    // clearInterval(interval);
+                    console.log(dynamicWidth);
+                    if (dynamicWidth > 99) {
+                        clearInterval(interval);
+                        clearInterval(secondInterval);
+                        clearTimeout(timeout);
+
+                    }
+                    resolve(res);
+                    // secondInterval = setInterval(() => {
+
+                    //     setDynamicWidth((val) => {
+                    //         const newVal = val + 1;
+
+                    //         if (newVal > 99) {
+
+
+                    //             clearInterval(secondInterval);
+                    //             clearTimeout(timeout);
+                    //         }
+                    //         return newVal;
+                    //     });
+                    // });
+
+
+                }, 10000000);
+            });
+            promise.then(res => {
+                clearInterval(interval);
+                clearInterval(secondInterval);
+                clearTimeout(timeout);
+                return res;
+            });
+            // timeout = setTimeout(() => {
+            //     // clearInterval(interval);
+            //     console.log(dynamicWidth);
+            //     if (dynamicWidth > 99) {
+            //         clearInterval(interval);
+            //         clearInterval(secondInterval);
+            //         clearTimeout(timeout);
+
+            //     }
+            //     // secondInterval = setInterval(() => {
+
+            //     //     setDynamicWidth((val) => {
+            //     //         const newVal = val + 1;
+
+            //     //         if (newVal > 99) {
+
+
+            //     //             clearInterval(secondInterval);
+            //     //             clearTimeout(timeout);
+            //     //         }
+            //     //         return newVal;
+            //     //     });
+            //     // });
+
+            //     return res;
+            // }, 10000000);
+
+            // clearTimeout(timeout);
+            // return res;
+        });
+        return () => {
+            // api.interceptors.request.eject(progressInterceptor);
+            api.interceptors.response.eject(progressEndInterceptor);
+            // clearInterval(interval);
+            // clearInterval(secondInterval);
+            // clearTimeout(timeout);
         }
     }, []);
 
